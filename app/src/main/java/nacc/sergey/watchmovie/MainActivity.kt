77e6623abad7fded074d02bada4d6c9dc5cdaad9
sergey_nacc <sergeynaksin@gmail.com>
2020-12-08 23:23:59 +0700

    added click animation

--- app/src/main/java/nacc/sergey/watchmovie/MainActivity.kt
+++ app/src/main/java/nacc/sergey/watchmovie/MainActivity.kt
@@ -1,10 +1,11 @@
 package nacc.sergey.watchmovie
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
