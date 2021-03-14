84dc394524a53eb79a3547f69ed8b3107a537441
SergeyNacc <sergeynaksin@gmail.com>
2021-03-14 17:49:32 +0300

    created Retrofit, Gson

---
+++ app/src/main/java/nacc/sergey/watchmovie/API.kt
@@ -0,0 +1,5 @@
+package nacc.sergey.watchmovie
+
+object API {
+    const val KEY = "8ac60a8fdd48e181c6518e14d98b0a86"
+}
--- app/src/main/java/nacc/sergey/watchmovie/App.kt
+++ app/src/main/java/nacc/sergey/watchmovie/App.kt
@@ -1,13 +1,21 @@
 package nacc.sergey.watchmovie
 
 import android.app.Application
+import nacc.sergey.watchmovie.data.ApiConstants
 import nacc.sergey.watchmovie.data.MainRepository
+import nacc.sergey.watchmovie.data.TmdbApi
 import nacc.sergey.watchmovie.domain.Interactor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class App : Application() {
 
     lateinit var repo: MainRepository
     lateinit var interactor: Interactor
+    lateinit var retrofitService: TmdbApi
 
     override fun onCreate(){
         super.onCreate()
@@ -18,8 +26,35 @@ class App : Application() {
         //Инициализируем репозиторий
         repo = MainRepository()
 
+        //Создаем кастомный клиент
+        val okHttpClient = OkHttpClient.Builder()
+        //таймауты для медленного интрнета
+            .callTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+        //логгер
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                if (BuildConfig.DEBUG) {
+                    level = HttpLoggingInterceptor.Level.BASIC
+                }
+            })
+            .build()
+
+        //Создаем ретрофит
+        val retrofit = Retrofit.Builder()
+
+        //Указываем базовый URL из констант
+            .baseUrl(ApiConstants.BASE_URL)
+        //Добавляем конвертер
+            .addConverterFactory(GsonConverterFactory.create())
+        //Добавляем кастомный клиент
+            .client(okHttpClient)
+            .build()
+
+        //Создаем сам сервис с методами для запросов
+        retrofitService = retrofit.create(TmdbApi::class.java)
+
         //Инициализируем интерактор
-        interactor = Interactor(repo)
+        interactor = Interactor(repo, retrofitService)
     }
 
     companion object {
