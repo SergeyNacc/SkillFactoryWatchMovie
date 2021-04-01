2ac814edbe7898bbc5c37f514cf6361904ce6939
SergeyNacc <sergeynaksin@gmail.com>
2021-04-01 12:17:07 +0300

    Koin

--- app/src/main/java/nacc/sergey/watchmovie/App.kt
+++ app/src/main/java/nacc/sergey/watchmovie/App.kt
@@ -1,67 +1,69 @@
 package nacc.sergey.watchmovie
 
 import android.app.Application
-import nacc.sergey.watchmovie.data.ApiConstants
-import nacc.sergey.watchmovie.data.MainRepository
-import nacc.sergey.watchmovie.data.TmdbApi
-import nacc.sergey.watchmovie.domain.Interactor
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
-
+import nacc.sergey.watchmovie.data.di.DI
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 class App : Application() {
 
-    lateinit var repo: MainRepository
-    lateinit var interactor: Interactor
-    lateinit var retrofitService: TmdbApi
+//    lateinit var repo: MainRepository
+//    lateinit var interactor: Interactor
+//    lateinit var retrofitService: TmdbApi
 
     override fun onCreate(){
         super.onCreate()
+        startKoin {
+            //Прикрепляем контекст
+            androidContext(this@App)
+            //(Опционально) подключаем зависимость
+            androidLogger()
+            //Инициализируем модули
+            modules(listOf(DI.mainModule))
+        }
 
-        //Инициализируем экземпляр App, через который будем получать доступ к остальным переменным
-        instance = this
-
-        //Инициализируем репозиторий
-        repo = MainRepository()
-
-        //Создаем кастомный клиент
-        val okHttpClient = OkHttpClient.Builder()
-        //таймауты для медленного интрнета
-            .callTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-        //логгер
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                if (BuildConfig.DEBUG) {
-                    level = HttpLoggingInterceptor.Level.BASIC
-                }
-            })
-            .build()
-
-        //Создаем ретрофит
-        val retrofit = Retrofit.Builder()
-
-        //Указываем базовый URL из констант
-            .baseUrl(ApiConstants.BASE_URL)
-        //Добавляем конвертер
-            .addConverterFactory(GsonConverterFactory.create())
-        //Добавляем кастомный клиент
-            .client(okHttpClient)
-            .build()
-
-        //Создаем сам сервис с методами для запросов
-        retrofitService = retrofit.create(TmdbApi::class.java)
-
-        //Инициализируем интерактор
-        interactor = Interactor(repo, retrofitService)
-    }
-
-    companion object {
-        //Здесь статически хранится ссылка на экземпляр App
-        lateinit var instance: App
-        //Приватный сеттер, чтобы нельзя было в эту переменную присвоить что-либо другое
-        private set
+//        //Инициализируем экземпляр App, через который будем получать доступ к остальным переменным
+//        instance = this
+//
+//        //Инициализируем репозиторий
+//        repo = MainRepository()
+//
+//        //Создаем кастомный клиент
+//        val okHttpClient = OkHttpClient.Builder()
+//        //таймауты для медленного интрнета
+//            .callTimeout(30, TimeUnit.SECONDS)
+//            .readTimeout(30, TimeUnit.SECONDS)
+//        //логгер
+//            .addInterceptor(HttpLoggingInterceptor().apply {
+//                if (BuildConfig.DEBUG) {
+//                    level = HttpLoggingInterceptor.Level.BASIC
+//                }
+//            })
+//            .build()
+//
+//        //Создаем ретрофит
+//        val retrofit = Retrofit.Builder()
+//
+//        //Указываем базовый URL из констант
+//            .baseUrl(ApiConstants.BASE_URL)
+//        //Добавляем конвертер
+//            .addConverterFactory(GsonConverterFactory.create())
+//        //Добавляем кастомный клиент
+//            .client(okHttpClient)
+//            .build()
+//
+//        //Создаем сам сервис с методами для запросов
+//        retrofitService = retrofit.create(TmdbApi::class.java)
+//
+//        //Инициализируем интерактор
+//        interactor = Interactor(repo, retrofitService)
+//    }
+//
+//    companion object {
+//        //Здесь статически хранится ссылка на экземпляр App
+//        lateinit var instance: App
+//        //Приватный сеттер, чтобы нельзя было в эту переменную присвоить что-либо другое
+//        private set
     }
 
 }
--- app/src/main/java/nacc/sergey/watchmovie/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/nacc/sergey/watchmovie/viewmodel/HomeFragmentViewModel.kt
@@ -2,15 +2,16 @@ package nacc.sergey.watchmovie.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import nacc.sergey.watchmovie.App
 import nacc.sergey.watchmovie.domain.Film
 import nacc.sergey.watchmovie.domain.Interactor
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
-class HomeFragmentViewModel : ViewModel() {
+class HomeFragmentViewModel : ViewModel(), KoinComponent {
     val filmsListLiveData: MutableLiveData<List<Film>> = MutableLiveData()
 
     //Инициализируем интерактор
-    private var interactor: Interactor = App.instance.interactor
+    private var interactor: Interactor by inject()
 
     init {
        interactor.getFilmsFromApi(1, object : ApiCallback {
